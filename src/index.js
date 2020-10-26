c7c988d1fd15701cc86287f08548a1fb2090c5c1
HarrisMBrad <bmichaelh13@gmail.com>
2020-10-26 11:16:51 -0400

    updated and reworked file-tree for routing.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  
+    <App />,  
+  
+  
   document.getElementById('root')
 );
+
 
 
