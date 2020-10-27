3801ad1375ef822a3bada34987384e2425508c30
HarrisMBrad <bmichaelh13@gmail.com>
2020-10-27 12:59:46 -0400

    updated menu-items to navigate withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 
 
 import './index.css';
@@ -9,11 +9,13 @@ import App from './App';
 
 ReactDOM.render(
 
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+     
   
-    <App />,  
   
-  
-  document.getElementById('root')
+  ,document.getElementById('root')
 );
 
 
