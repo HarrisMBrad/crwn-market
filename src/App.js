3faba6354fe1e8d63290953aaac2ddb3888193af
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-01 09:53:36 -0500

    adding redux, reducer, store and firebase utils

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
@@ -8,35 +9,31 @@ import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
-class App extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      currentUser: null
-    }
-  }
+class App extends React.Component {
+  
+  
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
+            setCurrentUser ({
               id: snapshot.id,
               ...snapshot.data()
-            }
+            
           });
-
-          // L 
         });
       }
 
-      this.setState({ currentUser: userAuth });
+      this.setState({ currentUser: userAuth});
     });
   }
 
@@ -47,7 +44,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser }/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} /> 
@@ -59,5 +56,7 @@ class App extends React.Component {
   }
   
 }
+
+
 
 export default App;
