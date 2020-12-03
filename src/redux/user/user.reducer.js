6d245392b0bbf95e21c8d0e14dfe75b8e89b8712
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-03 14:17:45 -0500

    ..

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const INITIAL_STATE = {
-    currentUser: null
-}
-
-const userReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case 'SET_CURRENT_USER':
-            return {
-                ...state,
-                currentUser: action.payload
-            }
-            
-        default:
-            return state;    
-        
-    }
-
-}
-
-export default userReducer;
-=======
 import { UserActionTypes } from './user.types';
 
 const INITIAL_STATE = {
@@ -39,4 +17,3 @@ const userReducer = (state = INITIAL_STATE, action) => {
 }
 
 export default userReducer;
->>>>>>> ded95278c04ccaec74854e318d3fa8350c16eb95
