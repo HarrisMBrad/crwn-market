3209e656389be8c04fa42069d9e4e8eb7991a226
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-03 14:15:29 -0500

    ..

--- src/index.js
+++ src/index.js
@@ -2,34 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
-
-=======
->>>>>>> ded95278c04ccaec74854e318d3fa8350c16eb95
 import './index.css';
 import App from './App';
 
 import store from './redux/store';
-<<<<<<< HEAD
-=======
 
 
->>>>>>> ded95278c04ccaec74854e318d3fa8350c16eb95
 
 
-<<<<<<< HEAD
-ReactDOM.render(
-<Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</Provider>
-  
-     
-  
-  
-  ,document.getElementById('root')
-=======
   <Provider store={store}>
 
   <BrowserRouter>
@@ -37,10 +17,9 @@ ReactDOM.render(
   </BrowserRouter>
 
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root');
 
->>>>>>> ded95278c04ccaec74854e318d3fa8350c16eb95
-);
+
 
 
 
