2f6a34bca4e9f60f2489a27900be3064ea9506ab
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-11 10:12:00 -0500

    updated checkout component, reducer and utility with equility checks

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selector';
 
 const Header = ({ currentUser, hidden }) => (
       <div className='header'>
