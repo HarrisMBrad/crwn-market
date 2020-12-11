53f5d4191c9288a5c714fed077b3057752298eb3
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-11 10:00:15 -0500

    correcting typos selector and reselect...

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselct';
+import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
