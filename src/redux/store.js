da5d62d8fcf869e5f2c80a211684f935a244a7b2
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-16 15:57:38 -0500

    ...

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,14 +8,11 @@ const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-<<<<<<< HEAD
+
 const persistor = persistStore(store);
 
 
 export default {store, persistor};
-=======
-export const persistor = persistStore(store);
 
 
-//export default { store, persistor };
->>>>>>> 6478b274429298ab1ca2be31a98ca658e2d6af7e
+ 
