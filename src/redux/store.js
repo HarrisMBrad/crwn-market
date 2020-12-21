cf0b0dfdb85dc24634763e183664112320487f3d
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-21 14:35:05 -0500

    exporting persistor object

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,16 +10,6 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
-<<<<<<< HEAD
-const persistor = persistStore(store);
-/**Why is this not exporting? */
-
-
-//export default {store, persistor}; // error message thinks its not exporting...?
-=======
-
-
 
 
  
->>>>>>> refs/remotes/origin/master
